4c5072f3372f282389a68492405d0ec667d47b74
Fabian Seitz <fabian.seitz@pari.com>
2026-03-11 11:33:41 +0100

    CI fixes

--- custom_components/foodsharing/coordinator.py
+++ custom_components/foodsharing/coordinator.py
@@ -4,6 +4,7 @@ import logging
 from typing import Any
 
 import async_timeout
+
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
